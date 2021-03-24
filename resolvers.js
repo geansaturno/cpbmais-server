081ccaa43a7e170d14bc26051b8f208261ce2617
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2021-03-24 18:47:42 -0300

    Comments removal

--- resolvers.js
+++ resolvers.js
@@ -5,16 +5,16 @@ const dateScalar = new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scalar type',
   serialize (value) {
-    return value.getTime() // Convert outgoing Date to integer for JSON
+    return value.getTime()
   },
   parseValue (value) {
-    return new Date(value) // Convert incoming integer to Date
+    return new Date(value)
   },
   parseLiteral (ast) {
     if (ast.kind === Kind.INT) {
-      return new Date(parseInt(ast.value, 10)) // Convert hard-coded AST string to integer and then to Date
+      return new Date(parseInt(ast.value, 10))
     }
-    return null // Invalid hard-coded value (not an integer)
+    return null
   }
 })
 
